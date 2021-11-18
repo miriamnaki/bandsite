d177f617e912094f5d020540a7f6f452274f74d2
MIRIAM NAKI <nakiyingim@gmail.com>
2021-11-17 22:37:01 -0800

    Added overlay to hero section

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,178 @@
+//shows section
+
+// Creating elements
+
+const concerts = [
+  {
+    date: 'Monday Sept 06 2021',
+    venue: 'Ronald Lane',
+    location: 'San Francisco, CA',    
+  },
+  {
+    date: 'Monday Sept 06 2021',
+    venue: 'Ronald Lane',
+    location: 'San Francisco, CA',
+    
+  },
+  {
+    date: 'Monday Sept 06 2021',
+    venue: 'Ronald Lane',
+    location: 'San Francisco, CA',
+    
+  }
+];
+const mainEl = document.querySelector('main');
+
+let sectionEl = document.createElement('section');
+mainEl.appendChild(sectionEl);
+sectionEl.classList.add('shows');
+
+let h1El = document.createElement('h1');
+sectionEl.appendChild(h1El);
+h1El.classList.add('shows__title');
+h1El.innerText = "Shows";
+
+let shows = document.querySelector('.shows')
+
+
+
+const show = document.querySelector('.shows');
+show.innerHTML += concerts.map(concert => 
+  `<div>
+  <p>date</p>
+  <p>${concert.date}</p>
+  <p>venue</p>
+  <p>${concert.venue}</p>
+  <p>location</p>
+  <p>${concert.location}</p>
+  </div>`
+).join(' ');
+
+ const shows = document.querySelector('.shows');
+ const allDivs = shows.querySelectorAll('div');
+ const btn = document.createElement('a');
+ shows.appendChild(btn)
+ allDivs.forEach((div)=> {
+  div.classList.add('shows__wrapper');
+ });
+ 
+const divs = document.querySelectorAll('.shows__wrapper');
+
+
+// console.log(divs)
+function addClass(n, newClass){
+for(let i = 0; i < divs.length; i++) {
+  let collection = divs[i].children
+  for(j =0; j< collection.length; j++){
+    let itemClass = collection[n]
+    itemClass.classList.add(newClass);
+  }
+}  
+}
+addClass(0, 'shows__label');
+addClass(1, 'shows__date');
+addClass(2, 'shows__label');
+addClass(3, 'shows__venue');
+addClass(4, 'shows__label');
+addClass(5, 'shows__location');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i= 0; i < concerts.length; i++) {
+//   // let concert = concerts[i];
+//   let concert = document.createElement('div');
+//   concert.classList.add('shows__wrapper')
+//   shows.appendChild(concert)
+
+//   // let data = document.createElement('p');
+//   // date.classList('shows__date');
+//   // date.
+// }
+
+// function createDiv(){}
+
+// let showOne = document.createElement('div');
+// sectionEl.appendChild(showOne);
+// showOne.classList.add('shows__wrapper');
+
+// let dateLabel = document.createElement('p');
+// let date = document.createElement('p');
+// let venueLabel = document.createElement('p');
+// let venue = document.createElement('p');
+// let locationLabel = document.createElement('p');
+// let locationEl = document.createElement('p');
+
+// showOne.appendChild(dateLabel);
+// dateLabel.classList.add('shows__label')
+// dateLabel.innerText = 'date'
+
+// showOne.appendChild(date);
+// date.classList.add('shows__label');
+// date.innerText = 'Mon Sept 06 2021';
+
+// showOne.appendChild(venueLabel);
+// date.classList.add('shows__label');
+// venueLabel.innerText = 'venue';
+
+// showOne.appendChild(venue);
+// venue.classList.add('shows__label');
+// venue.innerText = 'Roland Lane';
+
+// showOne.appendChild(locationLabel);
+// locationLabel.classList.add('shows__label');
+// locationLabel.innerText = 'location';
+
+// showOne.appendChild(locationEl);
+// locationEl.classList.add('location');
+// locationEl.innerText = 'San Fransisco, CA'
+
+
+
+
+
+// let showTwo = document.createElement('div');
+// sectionEl.appendChild(showTwo);
+// showTwo.classList.add('shows__wrapper');
+// console.log(showTwo)
+
+
+
+// let showThree = document.createElement('div');
+// sectionEl.appendChild(showThree);
+// showThree.classList.add('shows__wrapper');
+
+// let showFour = document.createElement('div');
+// sectionEl.appendChild(showFour);
+// showFour.classList.add('shows__wrapper');
+
+// let showFive = document.createElement('div');
+// sectionEl.appendChild(showFive);
+// showFive.classList.add('shows__wrapper');
+
+// let showSix = document.createElement('div');
+// sectionEl.appendChild(showSix);
+// showSix.classList.add('shows__wrapper');
+
+// let btn
+
