4553fd4bf60cf632a23167c92c2e55786d3aeb8a
MIRIAM NAKI <nakiyingim@gmail.com>
2021-11-20 12:41:54 -0800

    Added default comments with dom

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,19 +1,28 @@
 const comments = [
   {
-    avatar: "avatar",
+    avatar: {
+      url: "Mohan-muruge.jpg",
+      alt: "mohan"
+    },
     name: "Connor Walton",
     date: "02/17/2021",
     comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
 
   },
   {
-    avatar: "avatar",
+    avatar: {
+      url: "Mohan-muruge.jpg",
+      alt: "mohan"
+    },
     name: "Connor Walton",
     date: "01/09/2021",
     comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
   },
   {
-    avatar: "avatar",
+    avatar: {
+      url: "Mohan-muruge.jpg",
+      alt: "mohan"
+    },
     name: "Miles Acosta",
     date: "12/20/2020",
     comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
@@ -21,7 +30,64 @@ const comments = [
   }
 ];
 
-// const sectionEl = document.querySelector('.comments-section');
+const mainEl = document.querySelector('main');
+const sectionEl = document.createElement('section');
+sectionEl.classList.add('comments');
+mainEl.appendChild(sectionEl);
+console.log(sectionEl);
+
+comments.forEach((comment) => {
+
+const container = document.createElement('div');
+container.classList.add('comments__container');
+sectionEl.appendChild(container);
+console.log(mainEl);
+
+const flexOne = document.createElement('div');
+flexOne.classList.add('comments__flex-1');
+container.appendChild(flexOne)
+console.log(container);
+
+const commentOne = document.createElement('div');
+commentOne.classList.add('comments__one');
+flexOne.appendChild(commentOne);
+
+const avatar = document.createElement('img');
+avatar.classList.add('comments__avatar');
+avatar.src = '/assets/images/' + comment.avatar.url;
+avatar.alt =  comment.avatar.alt;
+console.log(avatar.alt)
+commentOne.appendChild(avatar);
+
+const commentName = document.createElement('p');
+commentName.classList.add('comments__name');
+commentName.innerHTML = comment.name
+commentOne.appendChild(commentName);
+console.log(container);
+
+const date = document.createElement('p');
+date.classList.add('comments__date');
+date.innerHTML = comment.date
+flexOne.appendChild(date);
+
+const flexTwo = document.createElement('div');
+flexTwo.classList.add('comments__flex-2');
+container.appendChild(flexTwo);
+
+const description = document.createElement('p');
+description.classList.add('comments__description');
+description.innerHTML = comment.comment;
+flexTwo.appendChild(description);
+
+const divider = document.createElement('hr');
+divider.classList.add('comments__divider');
+sectionEl.appendChild(divider);
+})
+
+
+
+
+
 // const defaultComments = document.createElement('div');
 // defaultComments.classList.add('comments__default');
 // sectionEl.appendChild(defaultComments);
@@ -38,10 +104,10 @@ const comments = [
 
 
 
-const form = document.querySelector(".comments");
+// const form = document.querySelector(".comments");
 
-form.addEventListener('submit', handleSubmitForm);
+// form.addEventListener('submit', handleSubmitForm);
 
-function handleSubmitForm(e) {
-  e.preventDefault();
-}
+// function handleSubmitForm(e) {
+//   e.preventDefault();
+// }
