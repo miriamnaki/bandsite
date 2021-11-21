c7f420c6f96412b24f75daaee1930ee6db2c125a
MIRIAM NAKI <nakiyingim@gmail.com>
2021-11-21 00:01:23 -0800

    Added show section using dom

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -2,26 +2,148 @@
 
 // Creating elements
 
-// const concerts = [
-//   {
-//     date: 'Monday Sept 06 2021',
-//     venue: 'Ronald Lane',
-//     location: 'San Francisco, CA',    
-//   },
-//   {
-//     date: 'Monday Sept 06 2021',
-//     venue: 'Ronald Lane',
-//     location: 'San Francisco, CA',
+const concerts = [
+  {
+    date: 'Monday Sept 06 2021',
+    venue: 'Ronald Lane',
+    location: 'San Francisco, CA',    
+  },
+  {
+    date: 'Tue Sept 21 2021',
+    venue: 'Pier 3 East ',
+    location: 'San Francisco, CA',
     
-//   },
-//   {
-//     date: 'Monday Sept 06 2021',
-//     venue: 'Ronald Lane',
-//     location: 'San Francisco, CA',
+  },
+  {
+    date: 'Fri Oct 15 2021',
+    venue: 'View Lounge ',
+    location: 'San Francisco, CA',
     
-//   }
-// ];
-// const mainEl = document.querySelector('main');
+  },
+  {
+    date: 'Sat Nov 06 2021 ',
+    venue: 'Hyatt Agency',
+    location: 'San Francisco, CA',
+    
+  },
+  {
+    date: 'Fri Nov 26 2021',
+    venue: 'Moscow Center  ',
+    location: 'San Francisco, CA',  
+  },
+  {
+    date: 'Wed Dec 15 2021 ',
+    venue: 'Press Club',
+    location: 'San Francisco, CA',   
+  }
+];
+
+// main
+const mainEl = document.querySelector('main');
+
+// article
+const article = document.createElement('article');
+article.classList.add('shows');
+mainEl.appendChild(article);
+console.log(article);
+
+// heading
+const h1 = document.createElement('h1');
+h1.classList.add('shows__title');
+h1.innerText = 'Shows';
+article.appendChild(h1);
+
+// shows container
+const container = document.createElement('div');
+container.classList.add('shows__container');
+article.appendChild(container);
+
+concerts.forEach((concert, index)=> {
+
+  // show wrapper
+  const showsWrapper = document.createElement('div');
+  showsWrapper.classList.add('shows__wrapper');
+  container.appendChild(showsWrapper)
+
+  // date wrapper
+  const dateWrapper = document.createElement('div');
+  dateWrapper.classList.add('shows__date-wrapper')
+  showsWrapper.appendChild(dateWrapper);
+
+  // date label
+  const dateLabel = document.createElement('p');
+  dateLabel.classList.add('shows__label');
+  if(index !== 0){
+    dateLabel.classList.add('shows__label--tablet-desktop')
+  }
+  dateLabel.innerText = 'date'
+  dateWrapper.appendChild(dateLabel);
+
+  // date
+  const date = document.createElement('p');
+  date.classList.add('shows__date');
+  date.innerHTML = concert.date;
+  dateWrapper.appendChild(date);
+
+  //venue wrapper
+  const venueWrapper = document.createElement('div');
+  venueWrapper.classList.add('shows__venue-wrapper');
+  showsWrapper.appendChild(venueWrapper);
+
+  //venue label
+  const venueLabel = document.createElement('p');
+  venueLabel.classList.add('shows__label');
+  if(index !== 0){
+    venueLabel.classList.add('shows__label--tablet-desktop')
+  }
+  venueLabel.innerText = 'venue'
+  venueWrapper.appendChild(venueLabel);
+
+  //venue
+  const venue = document.createElement('p');
+  venue.classList.add('shows__venue');
+  venue.innerHTML = concert.venue;
+  venueWrapper.appendChild(venue);
+
+  //location wrapper
+  const locationWrapper = document.createElement('div');
+  locationWrapper.classList.add('shows__location-wrapper');
+  showsWrapper.appendChild(locationWrapper);
+
+  // location label
+  const locationLabel = document.createElement('p');
+  locationLabel.classList.add('shows__label');
+  if(index !== 0){
+    locationLabel.classList.add('shows__label--tablet-desktop')
+  }
+  locationLabel.innerText = 'location';
+  locationWrapper.appendChild(locationLabel);
+
+  // location
+  const location = document.createElement('p');
+  location.classList.add('shows__location');
+  location.innerHTML = concert.location
+  locationWrapper.appendChild(location);
+
+  // button wrapper
+  const buttonWrapper = document.createElement('div');
+  buttonWrapper.classList.add('shows__button-wrapper');
+  buttonWrapper.classList.add('shows__button-wrapper--top')
+  showsWrapper.appendChild(buttonWrapper);
+
+  // button
+  const button = document.createElement('a');
+  button.classList.add('shows__booking');
+  button.setAttribute('href', "#");
+  button.innerText = "buy tickets"
+  buttonWrapper.appendChild(button);
+
+  // hr
+  const hr = document.createElement('hr');
+  container.appendChild(hr)
+
+
+})
 
 // let sectionEl = document.createElement('section');
 // mainEl.appendChild(sectionEl);
